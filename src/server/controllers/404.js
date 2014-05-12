6805e6f35beb568ff5ac99580c622e054210a047
Ulflander <xlaumonier@gmail.com>
2014-05-12 12:27:56 +0200

    Last changes

--- src/server/controllers/404.js
+++ src/server/controllers/404.js
@@ -12,7 +12,7 @@
      * @param  {Function} next Callback
      */
     exports.index = function(req, res, next) {
-        req.logger.error('Page not found: ' + req.url);
+        req.logger.warn('Page not found: ' + req.url);
         cv.view(req, res, '404.html');
     };
 
